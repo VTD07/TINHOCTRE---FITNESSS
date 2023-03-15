0b5286bcded213d50b17b7d6e37bfee26f491f81
VTD12 <alone24o61995@gmail.com>
2023-03-15 08:30:44 +0700

    second commit

--- HOMEPAGE.js
+++ HOMEPAGE.js
@@ -2,11 +2,31 @@ function openNav ()
 {
     document.getElementById("sidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.getElementById("open").style.visibility = "hidden";
 }
 function closeNav ()
 {
     document.getElementById("sidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
-    document.getElementById("open").style.visibility = "visible";
+}
+function menuFuction (x)
+{
+    x.classList.toggle("change");
+}
+function Menulist ()
+{
+    let an=true;
+    let hien=document.getElementById("sidenav");
+    let push=document.getElementById("main");
+    if(an)
+    {
+        document.getElementById("sidenav").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        an=false;
+    }
+    else
+    {
+        document.getElementById("sidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft= "0";
+        an=true;
+    }
 }
